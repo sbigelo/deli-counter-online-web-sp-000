ecffd3666a8166e19498b4b3bef3ec2ac96e4796
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-06 18:17:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+def line(katz_deli)
+  katz_deli.each_with_index do |num, index|
+  if num == num
+    
+    puts ("The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}")
+  else 
+  puts "The line is currently empty."
+    end
+  end
+end
