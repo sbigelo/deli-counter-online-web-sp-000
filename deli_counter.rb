bc2075eeea9abf5565d90a10fdd9cc39a226f46c
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-06 18:38:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli in line."
 end
